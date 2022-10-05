502ad07fe03c06858b00bdde9fb78fe0ba6e15a7
Matt Prentice <114755386+matt-prentice1@users.noreply.github.com>
2022-10-05 15:01:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "google" {
 resource "google_compute_instance" "pxe-server" {
   name         = "pxe-server"
   machine_type = "e2-medium"
-
+  min-cpu-platform="Intel Haswell"
 
   boot_disk {
     initialize_params {
@@ -35,10 +35,15 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = "true"
 }
 
+resource "google_compute_disk_resource_policy_attachment" "attachment"{
+  name = google_compute_resource_policy.policy.name
+  disk = google_compute_disk.standard.name
+  zone = "us-central1-c"
+}
+
 resource "google_compute_disk" "pxe-vol"{
   name = "pxe-vol"
   size = 100
   type = "pd-standard"
 }
- 
  
