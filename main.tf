602c779f1e693bac881331ed84955b0fd09e3111
Matt Prentice <114755386+matt-prentice1@users.noreply.github.com>
2022-10-05 13:38:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ provider "google" {
 resource "google_compute_instance" "pxe-server" {
   name         = "pxe-server"
   machine_type = "e2-medium"
-  --enable-nested-virtualization
 
 
   boot_disk {
